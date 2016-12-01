4866622da6698fd7bbdd5b5ecb86ffd3dd532045
DaMachinator <DaMachinator@users.noreply.github.com>
2016-12-01 15:51:12 -0500

    Update libmach.lua

--- lib/libmach.lua
+++ lib/libmach.lua
@@ -5,7 +5,7 @@ local cp = require("component")
 local event = require("event")
 local term = require("term")
 
-if not cp.isAvailable("gpu")
+if not cp.isAvailable("gpu") then
   error("NO GPU",0)
 end
 local gfx = cp.getPrimary("gpu")
@@ -21,9 +21,7 @@ local resWInit, resHInit = gfx.getResolution()
 local width = resWInit
 local heigth = resHInit
 
-
-
-
+--=======================--
 
 local libmach = {}
 
@@ -34,7 +32,7 @@ function libmach.autoRes()
   local maxAR = resWMax/(resHMax*2) --correct for characters being twice as tall as they are wide
   if scrAR == 1 then
     gfx.setResolution(resWMax,resWMax/2)
-  elseif scrAR > maxAR
+  elseif scrAR > maxAR then
     gfx.setResolution(resWMax,resWMax/(2*scrAR))
   else
     gfx.setResolution(resHMax*2*scrAR,resHMax)
@@ -44,4 +42,7 @@ end
 function libmach.err(msg)
   term.clear()
   gfx.setBackground(0xFFFFFF)
+  --todo: finish
 end
+
+return libmach
