3cbf1977c1d96695968e9bfa1d62c013c4c26230
DaMachinator <DaMachinator@users.noreply.github.com>
2017-09-23 13:41:10 -0400

    Create engine.lua

---
+++ extractor/engine.lua
@@ -0,0 +1,10 @@
+local engines = {}
+
+engines.Microturbine = 2097152
+engines.JetEngine = 67108864
+engines.GasEngine = 65536
+engines.ACEngine = 131072
+engines.PerformanceEngine = 262144
+engines.HydroEngine = 524288
+
+return(engines)
